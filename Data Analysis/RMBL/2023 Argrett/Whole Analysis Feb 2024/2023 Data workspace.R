aa85697915bc2e87c62ef40c503f6fc07ca84d60
Jargrett <77009555+Jargrett@users.noreply.github.com>
2024-02-13 09:53:42 -0500

    coding for RMBL

--- Data Analysis/RMBL/2023 Argrett/Whole Analysis Feb 2024/2023 Data workspace.R
+++ Data Analysis/RMBL/2023 Argrett/Whole Analysis Feb 2024/2023 Data workspace.R
@@ -15,6 +15,7 @@ library(ggpubr)
 library(emmeans) # for comparison of means
 library(rstatix) # for comparison of means
 library(labdsv)
+library(lme4)
 
 el.plants <- read.csv("Plant Data - Emerald Lake.csv")
 sp.plants <- read.csv("Plant Data - Schofield Park.csv")
@@ -29,4 +30,29 @@ sp.plot <- sp.plot[ -c(2,5,6,8:10)]
 comb.plot <- rbind(el.plot,sp.plot)
 comb.plants <- rbind(el.plants,sp.plants)
 comb.plant <- subset(comb.plants, comb.plants$Collection.Point=='Post')
+
+#-----------------Checking data structure-----------------#
+str(comb.plant)
+comb.plant$Code <- as.factor(comb.plant$Code)
+comb.plant$Functional.Group <- as.factor(comb.plant$Functional.Group)
+comb.plant$Removal <- as.factor(comb.plant$Removal)
+comb.plant$Life.History <- as.factor(comb.plant$Life.History)
+
+
+#------------Diversity Analysis------------#
+# here we will be working with the count column
+# we will need to conververt data to a matrix format
+comb.c <- subset(comb.plant, select = c('Plot','Code','Count'))
+comb.count<- matrify(comb.c)
+
+# Calculating the species richness for plots
+rich <- specnumber(comb.count)
+
+# Calculating Shannon diversity for plots
+div <- diversity(comb.count, index = "shannon")
+
+# Calculating species evenness for plots
+even <- diversity(comb.count, index = "shannon") / log(specnumber(comb.count))  
+comb.div <- cbind(comb.plot,div,rich,even)
+
 
