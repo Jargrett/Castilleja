65136e476434733261ca5d70fdf89525eac690ab
Jargrett <77009555+Jargrett@users.noreply.github.com>
2024-09-26 11:28:48 -0400

    gbif

---
+++ Data Analysis/Bot RA/GBIF Data.R
@@ -0,0 +1,25 @@
+#Species codes
+#initiated: 9/26/24
+
+install.packages("rgbif")
+library(rgbif)
+
+#Species codes:
+#Parthenium integrifolium
+#Silphium compositum
+#Packera anonyma
+#Asclepias tuberosa
+#Coreopsis major
+#Schizachyrium scoparium
+#Eryngium yuccifolium
+
+#taxon keys
+PAIN <- name_backbone("Parthenium integrifolium")$usageKey
+SICO <- name_backbone("Silphium compositum")$usageKey
+PAAN <- name_backbone("Packera anonyma")$usageKey
+ASTU <- name_backbone("Asclepias tuberosa")$usageKey
+COMA <- name_backbone("Coreopsis major")$usageKey
+SCSC <- name_backbone("Schizachyrium scoparium")$usageKey
+ERYU <- name_backbone("Eryngium yuccifolium")$usageKey
+
+
